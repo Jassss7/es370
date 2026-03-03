7f7525b9d0f596dc68b5c59983f1cf9cb0766f34
Jaas <jasserfridhi08@gmail.com>
2026-03-03 23:37:10 +0100

    j

--- LibreriaScuola/Scuola.cs
+++ LibreriaScuola/Scuola.cs
@@ -2,7 +2,40 @@
 {
     public class Scuola
     {
-        public List<Insegnante> Insegnanti { get; set; }
-        public List<Studente> Studenti { get; set; }
+        public List<Insegnante> Insegnanti { get; set; } = new List<Insegnante>();
+        public List<Studente> Studenti { get; set; } = new List<Studente>();
+        public List<Materia> Materie { get; set; } = new List<Materia>();
+        public float GetMedia(Studente studente)
+        {
+            if (studente.Voti.Count == 0) throw new Exception("errore,lo studente non ha voti.");
+            float somma = 0;
+         
+            foreach (var voto in studente.Voti)
+            {
+                
+               
+                    somma += voto.Valore;
+                    
+                
+            }
+            return somma / (float)studente.Voti.Count;
+        }
+        public float GetMediaAnno(Studente s , int anno)
+        {
+            if (s.Voti.Count == 0) throw new Exception("errore,lo studente non ha voti.");
+            float somma = 0;
+            int count = 0;
+            foreach (var voto in s.Voti)
+            {
+
+                if (voto.Data.Year == anno) { 
+                        somma += voto.Valore;
+                        count++;
+                }
+
+            }
+            if (count == 0) throw new Exception($"errore,lo studente non risulta avere voti nell'anno {anno}.");
+            return somma / count;
+        }
     }
 }
---
+++ TestProject1/Test1.cs
@@ -0,0 +1,61 @@
+﻿using LibreriaScuola;
+using Microsoft.Win32;
+using System.ComponentModel.DataAnnotations;
+namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+      
+        public void GetMedia_RitornaMediaCorretta()
+        {
+            Materia italiano = new Materia { Nome = "Italiano", Sigla = "IT" };
+            Materia edcivica = new Materia { Nome = "Eucazione civica", Sigla = "Ed.civica" };
+
+            var studente = new Studente
+            {
+                Voti = new List<Voto>
+                {
+                    new Voto { Valore = 6, Data = new DateTime(2024, 1, 10), Materia = italiano },
+                    new Voto { Valore = 8, Data = new DateTime(2026, 2, 10), Materia = edcivica }
+                }
+            };
+
+            Scuola scuola = new Scuola();
+
+            float risultato = scuola.GetMedia(studente);
+
+            Assert.AreEqual((float)7, risultato);
+        }
+
+        [TestMethod]
+        public void GetMediaAnno_RitornaMediaCorretta()
+        {
+            Materia italiano = new Materia { Nome = "Italiano", Sigla = "IT" };
+            Materia edcivica = new Materia { Nome = "Eucazione civica", Sigla = "Ed.civica" };
+
+            var studente = new Studente
+            {
+                Voti = new List<Voto>
+                {
+                    new Voto { Valore = 6, Data = new DateTime(2024, 1, 10), Materia = italiano },
+                    new Voto { Valore = 8, Data = new DateTime(2024, 2, 10), Materia = edcivica }
+                }
+            };
+
+            Scuola scuola = new Scuola();
+
+            float risultato = scuola.GetMediaAnno(studente, 2024);
+
+            Assert.AreEqual((float)7, risultato);
+        }
+
+
+    }
+    
+
+}
+    
+    
+
